e00498103e57bed1859209b40d7339296a0c2819
Sankalp <sankalp.m.1929@gmail.com>
2022-05-17 16:37:54 +0530

    forms added

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,11 @@
 from distutils.log import debug
+import logging
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '44d5e72e1e885b0fbe6c15c958bdcff6'
 
 posts = [
     {
@@ -24,6 +28,15 @@ def hello():
 def about():
     return render_template('about.html')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
 
 if __name__ == "__main__":
     app.run(debug = True)
