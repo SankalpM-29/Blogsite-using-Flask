9be2af8118d0eb7cfea872691758dfb56453cf5d
Sankalp <sankalp.m.1929@gmail.com>
2022-05-21 18:34:05 +0530

    picture updation method added

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,7 +2,7 @@ from email.mime import image
 from flask import request
 from flask_blog.models import User, Post
 from flask import render_template, url_for, flash, redirect
-from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_blog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,5 +70,17 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", 'success')
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename = "images/"+ current_user.image_file)
-    return render_template('account.html', title = "Account", image_file = image_file)
+    return render_template('account.html', title = "Account", image_file = image_file,
+                            form = form)
+    
