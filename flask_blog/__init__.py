3815352ffdab3c0adda27554e5befca55f0ac497
Sankalp <sankalp.m.1929@gmail.com>
2022-05-22 23:34:14 +0530

    blueprinting done

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,18 +1,38 @@
+import os
 from distutils.log import debug
 import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
- 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '44d5e72e1e885b0fbe6c15c958bdcff6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-db.create_all()
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+from flask_mail import Mail 
+from flask_blog.config import Config
+
+
+db = SQLAlchemy()
+# db.create_all()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from flask_blog import routes
+mail = Mail()
+
+
+def create_app(config_class = Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts
+    from flask_blog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- flask_blog/users/forms.py
+++ flask_blog/users/forms.py
@@ -1,11 +1,10 @@
-from xml.dom import ValidationErr
-from flask import Flask
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -59,7 +58,19 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That email is taken. Please choose a different one.')
 
 
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
